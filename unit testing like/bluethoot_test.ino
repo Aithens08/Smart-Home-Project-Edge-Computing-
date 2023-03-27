74371b1c99700753a53d0c2b7b6aa733f25551e8
Aithens08 <71881774+Aithens08@users.noreply.github.com>
2023-03-27 08:46:01 +0200

    Added the testing folder
    Folder for implementing short scripts use to test functionalities

---
+++ unit testing like/bluethoot_test.ino
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+#define bluetoothRX 50
+#define bluetoothTX 51
+SoftwareSerial mySerial(bluetoothRX, bluetoothTX);
+String message; //Message buffer
+
+char character; 
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
+  // Open serial communication for the Bluetooth Serial Port
+  mySerial.begin(115200);
+}
+
+void loop() {
+  // If any data is available at the Bluetooth Serial Port
+  if (mySerial.available()) {
+    Serial.write((mySerial.read()));
+  }
+
+  if (Serial.available()){
+    mySerial.write(Serial.read());
+  }
+}
